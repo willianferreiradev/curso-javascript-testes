888d3469a72194a0882a5e14dc16f279f953430b
Willian Ferreira <williannfferreira@gmail.com>
2022-04-02 01:10:49 -0300

    feat: add more cases to queryString

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,3 +1,9 @@
-module.exports.queryString = (obj) => {
-  return Object.keys(obj).map(value => `${value}=${obj[value]}`).join('&');
-}
+module.exports.queryString = (obj) =>
+  Object.entries(obj)
+    .map(([key, value]) => {
+      if (typeof value === 'object' && !Array.isArray(value)) {
+        throw new Error('Please check your params');
+      }
+      return `${key}=${value}`
+    })
+    .join('&');
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -9,4 +9,27 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Willian&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Fabio&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Fabio',
+      abilities: {
+        first: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 })
